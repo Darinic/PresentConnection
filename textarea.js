237d34cb6a4242c904fc3fdbaf15efc081d8c199
Darinic <97019341+Darinic@users.noreply.github.com>
2022-11-29 02:21:35 +0200

    fixing problems

--- textarea.js
+++ textarea.js
@@ -1,24 +1,55 @@
-function autosize(textarea_id, minRows, maxRows) {
-    let textarea = document.getElementById(textarea_id);
+function calculateLines() {
+    const cols = parseInt(this.getAttribute("cols"), null);
+    const text = this.value;
+    return Math.ceil(text.length / cols);
 
+}
+
+function regulateRowsAndOverflow({rows, minRows, maxRows, }) {
+    const lines = calculateLines.call(this);
+
+    if (rows > maxRows) {
+        this.setAttribute("rows", maxRows);
+        this.style.overflowY = "scroll";
+        this.scrollTop = this.scrollHeight;
+    } else if (lines >= minRows && lines <= maxRows) {
+        this.setAttribute("rows", lines);
+        this.style.overflowY = "hidden";
+    }
+}
+
+function getRows() {
+    return parseInt(this.getAttribute("rows"), null);
+}
+
+function setRows(rows) {
+    this.setAttribute("rows", rows);
+}
+
+function setInitialTextArea(textarea_id, minRows) {
+    const textarea = document.getElementById(textarea_id);
     textarea.setAttribute("rows", minRows);
+    return textarea;
+}
 
-    textarea.addEventListener("input", function () {
-        let rows = parseInt(this.getAttribute("rows"), 10);
+function autosize(textarea_id, minRows, maxRows) {
+    const textarea = setInitialTextArea(textarea_id, minRows);
+
+    textarea.addEventListener("keyup", function () {
+        let rows = getRows.call(this);
+
         while (this.scrollHeight > this.clientHeight) {
             if (rows < maxRows) {
                 rows++;
             } else {
                 ++rows;
             }
-            this.setAttribute("rows", rows);
+            setRows.call(this, rows);
         }
 
-        if (rows > maxRows) {
-            this.setAttribute("rows", maxRows);
-            this.style.overflowY = "scroll";
-        }
+        regulateRowsAndOverflow.call(this, {rows, minRows, maxRows, });
+       
     });
 }
 
-autosize("textarea", 2, 5);
+autosize("textarea", 2, 4);
